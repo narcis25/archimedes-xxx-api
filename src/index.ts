9d4487298fdbdefca1bc6848bd7664fd1c1fb3a1
narcis25 <narcis25@gmail.com>
2024-01-17 15:51:13 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
-import {initializeApp} from 'firebase-admin/app';
+//import {initializeApp } from 'firebase-admin/app';
 import {MessageRouter} from './router/MessageRouter';
 import {ALLOW_ORIGIN} from './const';
 import {PostRouter, UploadRouter} from './router';
+import admin from 'firebase-admin';
+
+//const serviceAccount = require("../../archimedes-fb-firebase-adminsdk-6lnps-841ff92e05.json");
+
+let serviceAccount = undefined;
+
+if( process.env.fKey != undefined ){
+  serviceAccount = JSON.parse(process.env.fKey);
+}else{
+  serviceAccount = require("../../archimedes-fb-firebase-adminsdk-6lnps-841ff92e05.json");
+}
 
 const app = express();
 app.use(
@@ -32,12 +43,19 @@ app.use(
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('combined'));
 
-initializeApp({
-  storageBucket: 'techhtml-blog.appspot.com',
-});
+const firebaseConfig = {
+  storageBucket: "archimedes-fb.appspot.com",
+  credential: admin.credential.cert(serviceAccount),
+};
+
+admin.initializeApp(firebaseConfig);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!!!????');
+});
+
+app.post('/', (req: Request, res: Response) => {
+  res.send('Hello World!!!????123124151234145');
 });
 
 app.use(MessageRouter);
--- src/router/MessageRouter/post.ts
+++ src/router/MessageRouter/post.ts
@@ -2,9 +2,23 @@ import {firestore} from 'firebase-admin';
 import {Request, Response} from 'express';
 
 export const postMessage = async (req: Request, res: Response) => {
-  const {message} = req.query;
+  //const {message} = req.query;
 
-  const ref = firestore().collection('message');
+  console.log(req.body);
+
+  //const message = req.body.params.data;
+
+  /*console.log(req.query);
+  console.log(message);*/
+
+  /*const ref = firestore().collection('message');
   await ref.add({message});
+  res.status(200).send('message added');*/
+
+  const ref = firestore().collection('message').doc('test');
+  await ref.set(req.body.params);
   res.status(200).send('message added');
+
+  /*await ref.add({message});
+  res.status(200).send('message added');*/
 };
