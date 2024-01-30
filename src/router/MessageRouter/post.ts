4d9a4db91f07cbd65aef7dcb1a410916397b6077
unknown <narcis25@gmail.com>
2024-01-31 00:03:07 +0900

    update

--- src/router/MessageRouter/post.ts
+++ src/router/MessageRouter/post.ts
@@ -60,7 +60,7 @@ export const postMessage = async (req: Request, res: Response) => {
 		await docRef.doc(String(new Date().getTime())).set(params.data);
 		res.status(200).send({res:'get_OKOK'});*/
 
-		async function post(){
+		/*async function post(){
 			const docRef = firestore().collection('items').doc(params.key).collection('contents');
 			await docRef.doc(String(new Date().getTime())).set(params.data).then(()=>{
 				res.status(200).send({res:'get_OKOK'});
@@ -73,14 +73,80 @@ export const postMessage = async (req: Request, res: Response) => {
 		.then((querySnapshot)=>{
 			params.data.ord = String(querySnapshot.size);
 			post();
+		});*/
+
+		if( params.data.name.length <= 0 ){
+			res.status(400).send({error:'공백으로 추가할 수 없습니다.'});
+			return;
+		}
+		
+
+		const docRef = firestore().collection('items').doc(params.key).collection('contents');
+		docRef
+		.where("col", "==", params.data.col)
+		.where("name", "==", params.data.name)
+		.where("type", "==", params.data.type)
+		.get()
+		.then(querySnapshot=>{
+			if( querySnapshot.size == 0 ){
+				// 값 넣는 것이 가능..
+
+				async function post(){
+					const docRef = firestore().collection('items').doc(params.key).collection('contents');
+					await docRef.doc(String(new Date().getTime())).set(params.data).then(()=>{
+						res.status(200).send({res:'get_OKOK'});
+					});
+				}
+
+				firestore().collection(`items/${params.key}/contents`)
+				.where("col", "==", params.data.col)
+				.get()
+				.then((querySnapshot)=>{
+					params.data.ord = String(querySnapshot.size);
+					post();
+				});
+			}else{
+				res.status(400).send({error:'동일한 항목이 이미 존재합니다.'});
+			}
 		});
 
 	}else if( query == 'remove' ){
-		firestore().collection(`items/${params.key}/contents`).doc(`${params.data.did}`).delete().then(()=>{
+		/*firestore().collection(`items/${params.key}/contents`).doc(`${params.data.did}`).delete().then(()=>{
 			res.status(200).send({res:'removeOK'});
 		}).catch((error)=>{
 			res.status(400).send(error);
+		});*/
+
+		
+		const docRef = firestore().collection(`items/${params.key}/contents`).doc(`${params.data.did}`);
+		docRef.get().then(doc=>{
+			const data:any = doc.data();
+			const type = data.col;
+			docRef.delete().then(()=>{
+
+				firestore().collection(`items/${params.key}/contents`)
+				.where("col", "==", type)
+				.orderBy("ord")
+				.get()
+				.then((querySnapshot)=>{
+					async function process(){
+						let ord:number = 0;
+						for( const doc of querySnapshot.docs ){
+							//console.log('arrange', doc.data());
+							if( doc.data().ord != ord ){
+								await doc.ref.update({ord:`${ord}`});
+							}
+							ord++;
+						}
+						res.status(200).send({res:'OKOK'});
+					}
+
+					process();
+				});
+			});			
 		});
+
+
 	}else if( query == 'switch' ){
 		const tgt_ord:number = params.data.tgt[0];
 		const tgt_index:number = params.data.tgt[1];
@@ -95,7 +161,31 @@ export const postMessage = async (req: Request, res: Response) => {
 		.orderBy("ord")
 		.get()
 		.then((querySnapshot)=>{
-			let ord:number = 0;
+			async function process(){
+				let ord:number = 0;
+
+				for( const doc of querySnapshot.docs ){
+					ord = Number(doc.data().ord);
+					if( tgt_ord == ord ){
+						await doc.ref.update({ord:`${tgt_index}`});
+					}else{
+						if( tgt_ord < tgt_index){
+							if( tgt_ord <= ord && ord <= tgt_index ){
+								if( tgt_index >= ord ){
+									await doc.ref.update({ord:`${ord-1}`});
+								}
+							}
+						}else{
+							if( tgt_index <= ord && ord <= tgt_ord ){
+								await doc.ref.update({ord:`${ord+1}`});
+							}
+						}
+					}
+				}
+				res.status(200).send({res:'OKOK'});
+			}
+			process();
+			/*let ord:number = 0;
 			let ok_cnt = 0;
 			let size:number = querySnapshot.size;
 			if( tgt_ord < tgt_index ) {
@@ -150,7 +240,7 @@ export const postMessage = async (req: Request, res: Response) => {
 						}
 					}
 				});
-			}
+			}*/
 			
 		});
 	}else if( query == 'arrange'){
@@ -160,7 +250,20 @@ export const postMessage = async (req: Request, res: Response) => {
 		.orderBy("ord")
 		.get()
 		.then((querySnapshot)=>{
-			let ord:number = 0;
+
+			async function process(){
+				let ord:number = 0;
+				for( const doc of querySnapshot.docs ){
+					console.log('arrange', doc.data());
+					await doc.ref.update({ord:`${ord}`});
+					ord++;
+				}
+				res.status(200).send({res:'OKOK'});
+			}
+
+			process();
+
+			/*let ord:number = 0;
 			let ok_cnt = 0;
 			let size:number = querySnapshot.size;
 			querySnapshot.forEach(doc=>{
@@ -172,12 +275,43 @@ export const postMessage = async (req: Request, res: Response) => {
 					}
 				})
 				ord++;
-			});
+			});*/
+
+			
+
+
 		});
 	}else if( query == 'edit' ){
-		const ref = firestore().collection(`items/${params.key}/contents`).doc(`${params.data.did}`);
+		if( params.data.name.length <= 0 ){
+			res.status(400).send({error:'공백으로 추가할 수 없습니다.'});
+			return;
+		}
+
+		const docRef = firestore().collection('items').doc(params.key).collection('contents');
+		docRef
+		.where("col", "==", params.data.col)
+		.where("name", "==", params.data.name)
+		.where("type", "==", params.data.type)
+		/*.where("ord", "!=", params.data.ord)*/
+		.get()
+		.then(querySnapshot=>{
+			if( querySnapshot.size == 0 ){
+				// 값 넣는 것이 가능..
+				async function action(){
+					const ref = firestore().collection(`items/${params.key}/contents`).doc(`${params.data.did}`);
+					await ref.set(params.data);
+					res.status(200).send({res:'OKOK'});
+				}
+				action();
+			}else{
+				res.status(400).send({error:'동일한 항목이 이미 존재합니다.'});
+			}
+		});
+
+
+		/*const ref = firestore().collection(`items/${params.key}/contents`).doc(`${params.data.did}`);
 		await ref.set(params.data);
-		res.status(200).send({res:'OKOK'});
+		res.status(200).send({res:'OKOK'});*/
 	}
 
   //const {message} = req.query;
